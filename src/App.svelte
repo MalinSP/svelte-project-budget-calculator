d0a11e96be67c88c1cf10f64bc47b730a6390e84
Sherlok <sergei_malin@yahoo.com>
2022-04-24 15:24:25 -0500

    add functions

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,67 @@
 <script>
-import Navbar from "./Navbar.svelte";
-import ExpensesList from "./ExpenseList.svelte"
+  import expensesData from './expenses'
+  let expenses = [...expensesData]
 
-import expensesData from "./expenses"
-let expenses = [...expensesData]
+  let setName = ''
+  let setAmount = null
+  let setId = null
+
+  $: isEditing = setId ? true : false
+  $: total = expenses.reduce((acc, curr) => {
+    return (acc += curr.amount)
+  }, 0)
+
+  import { setContext } from 'svelte'
+
+  import Navbar from './Navbar.svelte'
+  import ExpensesList from './ExpenseList.svelte'
+  import Totals from './Totals.svelte'
+  import FormExpense from './FormExpense.svelte'
+
+  function removeExpense(id) {
+    expenses = expenses.filter((item) => item.id !== id)
+  }
+  function clearexpenses() {
+    expenses = []
+  }
+  function addExpense({ name, amount }) {
+    let expense = { id: Math.random() * Date.now(), name, amount }
+    expenses = [expense, ...expenses]
+  }
+  function setModifiedExpense(id) {
+    let expense = expenses.find((item) => item.id === id)
+    setId = expense.id
+    setName = expense.name
+    setAmount = expense.amount
+  }
+  function editExpense({ name, amount }) {
+    expenses = expenses.map((item) => {
+      return item.id === setId ? { ...item, name, amount } : { ...item }
+    })
+    setId = null
+    setAmount = null
+    setName = ''
+  }
+
+  setContext('remove', removeExpense)
+  setContext('modify', setModifiedExpense)
 </script>
 
 <Navbar />
 <main class="content">
-	<ExpensesList />
+  {isEditing}
+  <FormExpense
+    {addExpense}
+    name={setName}
+    amount={setAmount}
+    {isEditing}
+    {editExpense}
+  />
+  <Totals title="total expenses" {total} />
+  <ExpensesList {expenses} />
+  <button
+    type="button"
+    class="btn btn-primary btn-block"
+    on:click={clearexpenses}>clear expense</button
+  >
 </main>
-
--- src/Expense.svelte
+++ src/Expense.svelte
@@ -1,1 +1,39 @@
-<h1>this is single expense </h1>
+<script>
+  import { getContext } from 'svelte'
+  // console.log(expense);
+  // let {name, amount, id} = expense
+  export let id
+  export let name = ''
+  export let amount = 0
+
+  let displayAmount = false
+
+  function toggleAmount() {
+    displayAmount = !displayAmount
+  }
+  const removeExpense = getContext('remove')
+  const setModifiedExpense = getContext('modify')
+</script>
+
+<article class="single-expense">
+  <div class="expense-info">
+    <h2>
+      {name}
+      <button class="amount-btn" on:click|once={toggleAmount}>
+        <i class="fas fa-caret-down" />
+      </button>
+    </h2>
+    {#if displayAmount}<h4>amount : ${amount}</h4>{/if}
+  </div>
+  <div class="expense-buttons">
+    <button
+      class="expense-btn edit-btn"
+      on:click={() => setModifiedExpense(id)}
+    >
+      <i class="fas fa-pen" />
+    </button>
+    <button class="expense-btn delete-btn" on:click={() => removeExpense(id)}>
+      <i class="fas fa-trash" />
+    </button>
+  </div>
+</article>
